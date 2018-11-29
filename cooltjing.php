3630ab6e3b886b7eda239b618abe3c7d1ca6c0b4
Sidas Snieska <ssnieskiokas@gmail.com>
2018-11-29 16:03:15 +0100

    Initial commit

---
+++ cooltjing.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sidassnieska
+ * Date: 29/11/2018
+ * Time: 15:42
+ */
+sdfdsf
+
+sdf
+dsf
+dsfs
+dfds
+foreach (df as sdfds) {
+    
+}
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sidassnieska
+ * Date: 29/11/2018
+ * Time: 13:51
+ */
+
+echo "Hello World!";
+
+echo "Nothing to see here";
+
+echo "Yep news";
+
+
+echo "wtf";
+
+
+
+echo "wtf";
+
+echo "wtf";
+
+echo "wtf";
+
+v
